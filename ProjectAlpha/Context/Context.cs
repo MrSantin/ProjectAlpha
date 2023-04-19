5c0846c498d9af0c5b10a584131ea4486c3477b2
Marcos Santin <mrsantin.ms@gmail.com>
2023-04-19 00:10:02 -0300

    feat: Adiciona classe dbcontext

---
+++ ProjectAlpha/Context/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAlpha.Entities;
+
+namespace ProjectAlpha.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+        {
+
+
+        }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Message>().Property(x => x.Id);
+            modelBuilder.Entity<Project>().Property(x => x.Id);
+            modelBuilder.Entity<Project>().HasMany(x => x.Activities);
+            modelBuilder.Entity<Staff>().Property(x => x.Id);
+            modelBuilder.Entity<Activity>().Property(x => x.Id);
+        }
+    }
+}
