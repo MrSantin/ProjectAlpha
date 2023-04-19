880a151a6669dfe359feffb0e1083c3f15e57b6c
Marcos Santin <mrsantin.ms@gmail.com>
2023-04-19 00:12:51 -0300

    refat: Ajusta o mappin da propriedade Activity

--- ProjectAlpha/Entities/Project.cs
+++ ProjectAlpha/Entities/Project.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Task> Tasks { get; set; }
+        public List<Activity> Activities { get; set; }
         public Staff ProjectOwner { get; set; }
 
         public Project() { 
