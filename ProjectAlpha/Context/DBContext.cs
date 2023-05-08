28c565b8b1933b17f724cd7818662372cd4a9d49
Marcos Santin <mrsantin.ms@gmail.com>
2023-05-08 20:06:12 -0300

    Ajusta o nome da classe DBContext para não conflitar com tipos de dados do projeto

--- ProjectAlpha/Context/DBContext.cs
+++ ProjectAlpha/Context/DBContext.cs
@@ -3,17 +3,14 @@ using ProjectAlpha.Entities;
 
 namespace ProjectAlpha.Context
 {
-    public class Context : DbContext
+    public class DBContext : DbContext
     {
-        public Context(DbContextOptions<Context> options)
-        {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
 
-
-        }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Project> Projects { get; set; }
-        public DbSet<Staff> Staffs { get; set; }
-        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Message> Staffs { get; set; }
+        public DbSet<Message> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,8 +18,8 @@ namespace ProjectAlpha.Context
             modelBuilder.Entity<Message>().Property(x => x.Id);
             modelBuilder.Entity<Project>().Property(x => x.Id);
             modelBuilder.Entity<Project>().HasMany(x => x.Activities);
-            modelBuilder.Entity<Staff>().Property(x => x.Id);
-            modelBuilder.Entity<Activity>().Property(x => x.Id);
+            modelBuilder.Entity<Message>().Property(x => x.Id);
+            modelBuilder.Entity<Message>().Property(x => x.Id);
         }
     }
 }
