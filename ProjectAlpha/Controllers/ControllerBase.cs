02f16848777bd9e742ce0a9dec4f21656e784662
Marcos Santin <mrsantin.ms@gmail.com>
2023-05-08 20:09:53 -0300

    Adiciona uma controllerbase para as demais controllers

---
+++ ProjectAlpha/Controllers/ControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectAlpha.Entities;
+using ProjectAlpha.Repository;
+
+namespace ProjectAlpha.Controllers
+{
+    [ApiController]
+    public class ControllerBase<T> : ControllerBase where T : class, IEntity
+    {
+        private readonly IRepository<T> _repository;
+
+        public ControllerBase(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        // Implemente os métodos CRUD aqui
+    }
+}
